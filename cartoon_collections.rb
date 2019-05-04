78f85ac60bfbcad707fb4634e7d3a108b12f2ac9
William Bainton <baintonw@gmail.com>
2019-05-04 01:22:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey"]
+def roll_call_dwarves(dwarves)
+  dwarves.each do |dwarf|
+    puts "#{dwarves.index(dwarf) + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(calls)
+  calls.map do |word|
+    word.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
